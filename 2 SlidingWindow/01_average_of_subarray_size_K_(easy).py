85ec65a661b2ad5616f456b69e629685adad13aa
ADMIN\admin <Ihtyyarovich@gmail.com>
2023-11-07 21:21:03 +0300

    simple binary search

--- 2 SlidingWindow/01_average_of_subarray_size_K_(easy).py
+++ 2 SlidingWindow/01_average_of_subarray_size_K_(easy).py
@@ -1,4 +1,9 @@
 
+from typing import List
+
+
+def find_average_of_subarrays(nums: List, k: int):
+
 
 if __name__ == '__main__':
     print('Averages of sub arrays of size K: ' + str(find_averages_of_subarrays([1, 3, 2, 6, -1, 4, 1, 8, 2], 5)))
