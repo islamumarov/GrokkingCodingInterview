3267a89c6d72ef412965bfade140d25807f87fac
ADMIN\admin <Ihtyyarovich@gmail.com>
2023-11-19 20:51:00 +0300

    leetcode first task

--- TwoPointers/01_pair_with_target_sum(easy).py
+++ TwoPointers/01_pair_with_target_sum(easy).py
@@ -1,4 +1,14 @@
-
+def search(arr, target_sum):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target_sum:
+            return [left, right]
+        if target_sum > current_sum:
+            left += 1
+        else:
+            right -= 1
+    return [-1, -1]
 
 if __name__ == '__main__':
     print(str(search([2, 7, 11, 15], 9)))
