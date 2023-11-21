937a47e50f7deaf8c32e438ad80d72260e9927af
ADMIN\admin <Ihtyyarovich@gmail.com>
2023-11-21 17:38:30 +0300

    the smallest contiguous subarray whose sum is greater than or equal to ‘S’

--- 2 SlidingWindow/03_smallest_subarray_with_a_given_sum_(easy).py
+++ 2 SlidingWindow/03_smallest_subarray_with_a_given_sum_(easy).py
@@ -1,3 +1,21 @@
+#Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. 
+# Return 0, if no such subarray exists.
+
+
+def smallest_subarray_with_given_sum(s, arr):
+    window_sum = 0
+    min_length = float('inf')
+    window_start = 0
+    for window_end in range(0, len(arr)):
+        window_sum += arr[window_end]
+        while window_sum >= s:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    if min_length == float('inf'):
+        return 0
+    return min_length
+    
 
 
 if __name__ == '__main__':
