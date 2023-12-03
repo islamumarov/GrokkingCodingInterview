680dc96c47dfb2a1faf98c7b1efe93b5256ea216
ADMIN\admin <Ihtyyarovich@gmail.com>
2023-12-03 16:56:40 +0300

    added The longest substring for aabccbb without any repeating characters

--- 2 SlidingWindow/06_no_repeat_substring_(hard).py
+++ 2 SlidingWindow/06_no_repeat_substring_(hard).py
@@ -1,4 +1,15 @@
-
+def non_repeat_substring(str):
+    dict = {}
+    max_len = 0
+    window_start = 0
+    for i, j in enumerate(str):
+        while j in dict:
+            del dict[str[window_start]]
+            window_start += 1
+        if j not in dict:
+            dict[j] = 1
+        max_len = max(max_len, len(dict))
+    return max_len
 
 
 if __name__ == '__main__':
@@ -7,7 +18,3 @@ if __name__ == '__main__':
     print("The longest substring for abbbb without any repeating characters is " + str(non_repeat_substring("abbbb")))
     print("The longest substring for abccde without any repeating characters is " + str(non_repeat_substring("abccde")))
     # second approach
-
-    print("The longest substring for aabccbb without any repeating characters is " + str(non_repeat_substring_2("aabccbb")))
-    print("The longest substring for abbbb without any repeating characters is " + str(non_repeat_substring_2("abbbb")))
-    print("The longest substring for abccde without any repeating characters is " + str(non_repeat_substring_2("abccde")))
